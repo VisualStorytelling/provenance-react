902b0294c4237a9ff77b28141f8b0de234b86606
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-05-18 11:44:50 +0200

    Use core 1.1.0

--- src/ProvenanceGraph.tsx
+++ src/ProvenanceGraph.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { IProvenanceGraphTraverser } from '@visualstorytelling/provenance-core'
+import { IProvenanceGraphTraverser, NodeIdentifier } from '@visualstorytelling/provenance-core'
 import { ProvenanceGraphNode } from './provenance-react';
 
 export interface IProps {
@@ -7,14 +7,17 @@ export interface IProps {
 }
 
 export class ProvenanceGraph extends React.Component<IProps, {}> {
-    onClick = () => {
-
+    onClick = (nodeIdentifier: NodeIdentifier) => {
+        this.props.provenance.toStateNode(nodeIdentifier).then(() => {
+            // do something after node has been made current
+        });
     }
 
     render() {
+        const root = this.props.provenance.graph.root
         return (
             <ol>
-                <ProvenanceGraphNode node={this.props.graph.root} onClick={this.onClick} />
+                <ProvenanceGraphNode node={root} onClick={this.onClick} />
             </ol>
         )
     }
