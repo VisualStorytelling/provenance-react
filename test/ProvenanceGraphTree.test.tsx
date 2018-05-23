9a228db5a5c4e10545c5db842284ee4ae099f9e1
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-05-23 11:29:00 +0200

    More tests to make coverage green

--- test/ProvenanceGraphTree.test.tsx
+++ test/ProvenanceGraphTree.test.tsx
@@ -2,14 +2,93 @@ import * as React from 'react'
 
 import { shallow } from 'enzyme'
 import { ProvenanceGraphTree } from '../src/ProvenanceGraphTree'
-import { ProvenanceGraph, ActionFunctionRegistry, ProvenanceGraphTraverser } from '@visualstorytelling/provenance-core';
+import { ProvenanceGraph, ActionFunctionRegistry, ProvenanceGraphTraverser, IProvenanceGraphTraverser, IProvenanceGraph, IActionFunctionRegistry } from '@visualstorytelling/provenance-core';
 
 describe('ProvenanceGraphTree', () => {
-    test('should render', () => {
-        const registry = new ActionFunctionRegistry();
-        const graph = new ProvenanceGraph({name:'test', version:'1.2.3'})
-        const traverser = new ProvenanceGraphTraverser(registry, graph);
+    let registry : IActionFunctionRegistry;
+    let graph: IProvenanceGraph;
+    let traverser : IProvenanceGraphTraverser
 
-        const wrapper = shallow(<ProvenanceGraphTree traverser={traverser}/>)
+    beforeEach(() => {
+        registry = new ActionFunctionRegistry();
+        graph = new ProvenanceGraph({name:'test', version:'1.2.3'})
+        traverser = new ProvenanceGraphTraverser(registry, graph);
     })
-});
+
+    test('should render', () => {
+        shallow(<ProvenanceGraphTree traverser={traverser}/>)
+    })
+
+    describe('componentDidMount()', () => {
+        let spy;
+        let tree: ProvenanceGraphTree
+
+        beforeEach(() => {
+            spy = spyOn(graph, 'on')
+            tree = new ProvenanceGraphTree({traverser});
+            tree.componentDidMount();
+        })
+
+        it('should subscribe to changes to graph', () => {
+            expect(spy).toBeCalledWith('nodeAdded', expect.any(Function));
+            expect(spy).toBeCalledWith('currentChanged', expect.any(Function));
+            expect(spy).toBeCalledWith('nodeChanged', expect.any(Function));
+        })
+    })
+
+    describe('componentWillUnmount()', () => {
+        let spy;
+        let tree: ProvenanceGraphTree
+
+        beforeEach(() => {
+            spy = spyOn(graph, 'off')
+            tree = new ProvenanceGraphTree({traverser});
+            tree.componentWillUnmount();
+        })
+
+        it('should unsubscribe to changes to graph', () => {
+            expect(spy).toBeCalledWith('nodeAdded', expect.any(Function));
+            expect(spy).toBeCalledWith('currentChanged', expect.any(Function));
+            expect(spy).toBeCalledWith('nodeChanged', expect.any(Function));
+        })
+    })
+
+    describe('onGraphChange()', () => {
+        let spy
+        let tree: ProvenanceGraphTree
+
+        beforeEach(() => {
+            tree = new ProvenanceGraphTree({traverser});
+            spy = spyOn(tree, 'forceUpdate')
+        })
+
+        it('should call forceUpdate', () => {
+            tree.onGraphChange();
+
+            expect(spy).toBeCalled()
+        })
+    })
+
+    describe('onClick()', () => {
+        let mock
+        let spy
+        let tree: ProvenanceGraphTree
+
+        beforeEach(() => {
+            tree = new ProvenanceGraphTree({traverser})
+            mock =  jest.fn(() => Promise.resolve())
+            traverser.toStateNode = mock
+            spy = spyOn(tree, 'forceUpdate')
+
+            return tree.onClick('some-id')
+        })
+
+        it('should call traverser.toStateNode', () => {
+            expect(mock).toBeCalledWith('some-id')
+        })
+
+        it('should call forceUpdate', () => {
+            expect(spy).toBeCalled()
+        })
+    })
+})
